6f7f2fe73a077d9310b53af2fb9a2903d5c5dcf9
Anson Hunnell <ansonhunnell@gmail.com>
2023-05-17 15:49:30 -0400

    initial commit to main branch

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const submissions = document.querySelectorAll(".submissions > div")
 const dragAreas = document.querySelectorAll(".drag-area")
+
+submissions.forEach(submission => {
+  submission.addEventListener("dragstart", e => {
+    submission.classList.add("dragging")
+  })
+})
