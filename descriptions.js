bb8d607edde1ca152c0bf7c68835f8438df7424a
Anson Hunnell <ansonhunnell@gmail.com>
2023-08-23 12:05:04 -0400

    add mobile functionality to this app

--- descriptions.js
+++ descriptions.js
@@ -6,25 +6,41 @@ function descriptions() {
   const subDescription = document.querySelector(".sub-description")
   if (subInfoModal.open) {
     if (subInfoModalTitle.innerText === "triangle") {
-      subDescription.innerText = "Description for triangle"
-      subInfoLink.innerHTML = "Link for triangle"
-      subInfoLink.setAttribute("href", "https://example.com/triangle")
+      subDescription.innerText =
+        "What makes the triangle awesome? This strangle pits the strength of a persons legs against an opponent's upper body which makes it very secure indeed when fully locked. Even larger opponents will find it extremely difficult and miserable to escape a fully-locked triangle before being rendered unconscious. What keeps this technique out of S-tier is the fact it is a head and arm style strangle and also the potential risk of being slammed."
+      subInfoLink.innerHTML = "triangle video"
+      subInfoLink.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=WsOvsDR_104"
+      )
     } else if (subInfoModalTitle.innerText === "arm triangle") {
-      subDescription.innerText = "Description for arm-triangle"
+      subDescription.innerText = "arm triangle video"
       subInfoLink.innerHTML = "Link for arm-triangle"
-      subInfoLink.setAttribute("href", "https://example.com/arm-triangle")
+      subInfoLink.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=Tnj3UNVnMU0&t=96s"
+      )
     } else if (subInfoModalTitle.innerText === "guillotine") {
       subDescription.innerText = "Description for guillotine"
-      subInfoLink.innerHTML = "Link for guillotine"
-      subInfoLink.setAttribute("href", "https://example.com/guillotine")
+      subInfoLink.innerHTML = "guillotine video"
+      subInfoLink.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=0FO10dH_3RU"
+      )
     } else if (subInfoModalTitle.innerText === "heel-hook") {
       subDescription.innerText = "Description for heel-hook"
-      subInfoLink.innerHTML = "Link for heel hook"
-      subInfoLink.setAttribute("href", "https://example.com/heel-hook")
+      subInfoLink.innerHTML = "heel hook video"
+      subInfoLink.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=haBJPddcCvo"
+      )
     } else if (subInfoModalTitle.innerText === "armbar") {
       subDescription.innerText = "Description for armbar"
-      subInfoLink.innerHTML = "Link for armbar"
-      subInfoLink.setAttribute("href", "https://example.com/armbar")
+      subInfoLink.innerHTML = "armbar video"
+      subInfoLink.setAttribute(
+        "href",
+        "https://www.youtube.com/watch?v=GshEzcqlUbY&t=34s"
+      )
     } else if (subInfoModalTitle.innerText === "rnc") {
       subDescription.innerText = "this move is fucking dope"
       subInfoLink.setAttribute(
--- script.js
+++ script.js
@@ -84,12 +84,138 @@ function submitButtonGraphics() {
 draggables.forEach(draggable => {
   draggable.addEventListener("dragstart", e => {
     const itemId = draggable.classList[1]
-    e.dataTransfer.setData("text/plain", itemId)
+    // e.dataTransfer.setData("text/plain", itemId)
     draggable.classList.add("dragging")
   })
 
   draggable.addEventListener("dragend", e => {
     draggable.classList.remove("dragging")
+  })
+})
+
+// mobile touch logic----------------------------------------------------------------
+// if ("ontouchstart" in window) {
+// }
+draggables.forEach(draggable => {
+  draggable.addEventListener("touchstart", e => {
+    e.stopPropagation()
+    draggables.forEach(item => {
+      if (item !== draggable && item.classList.contains("dragging")) {
+        item.classList.remove("dragging")
+      }
+    })
+    draggable.classList.toggle("dragging")
+  })
+})
+
+// mobile sorting logic--------------------------------------------------------------------------------
+containers.forEach(container => {
+  container.addEventListener("touchstart", e => {
+    const afterElement = sortDraggableElements(container, e.clientX)
+    const draggable = document.querySelector(".dragging")
+
+    if (container.id != "bank") {
+      if (afterElement == null) {
+        container.appendChild(draggable)
+        draggable.classList.remove("dragging")
+      } else {
+        container.insertBefore(draggable, afterElement)
+        draggable.classList.remove("dragging")
+      }
+    } else if (container.id == "bank") {
+      container.appendChild(draggable)
+      draggable.classList.remove("dragging")
+    }
+
+    const draggableItems = Array.from(container.querySelectorAll(".draggable"))
+
+    draggableItems.forEach((draggableItem, index) => {
+      const rankValue = draggableItem.dataset.rank
+      const indexValue = index.toString()
+
+      if (container.id != "bank") {
+        draggableItem.classList.remove("unplaced")
+        draggableItem.classList.add("placed")
+      } else {
+        draggableItem.classList.remove("placed")
+        draggableItem.classList.add("unplaced")
+      }
+
+      if (
+        indexValue === rankValue &&
+        container.id === draggableItem.classList[1]
+      ) {
+        if (
+          index > 0 &&
+          draggableItems[index - 1].dataset.order !== "true" &&
+          draggableItem.dataset.order === "true"
+        ) {
+          draggableItem.dataset.order = ""
+          draggableItem.dataset.tier = "true"
+          draggableItem.dataset.score = 1
+        } else {
+          draggableItem.dataset.order = "true"
+          draggableItem.dataset.tier = "true"
+          draggableItem.dataset.score = 2
+        }
+      } else if (
+        indexValue != rankValue &&
+        container.id === draggableItem.classList[1]
+      ) {
+        draggableItem.dataset.order = ""
+        draggableItem.dataset.tier = "true"
+        draggableItem.dataset.score = 1
+      } else {
+        draggableItem.dataset.order = ""
+        draggableItem.dataset.tier = ""
+        draggableItem.dataset.score = 0
+      }
+    })
+    containers.forEach(container => {
+      const draggableItems = Array.from(
+        container.querySelectorAll(".draggable")
+      )
+
+      draggableItems.forEach((draggableItem, index) => {
+        const rankValue = draggableItem.dataset.rank
+        const indexValue = index.toString()
+
+        if (
+          indexValue === rankValue &&
+          container.id === draggableItem.classList[1]
+        ) {
+          if (
+            index > 0 &&
+            draggableItems[index - 1].dataset.order !== "true" &&
+            draggableItem.dataset.order === "true"
+          ) {
+            draggableItem.dataset.order = ""
+            draggableItem.dataset.tier = "true"
+            draggableItem.dataset.score = 1
+          } else {
+            draggableItem.dataset.order = "true"
+            draggableItem.dataset.tier = "true"
+            draggableItem.dataset.score = 2
+          }
+        } else if (
+          indexValue != rankValue &&
+          container.id === draggableItem.classList[1]
+        ) {
+          draggableItem.dataset.order = ""
+          draggableItem.dataset.tier = "true"
+          draggableItem.dataset.score = 1
+        } else {
+          draggableItem.dataset.order = ""
+          draggableItem.dataset.tier = ""
+          draggableItem.dataset.score = 0
+        }
+      })
+    })
+    submitButtonGraphics()
+
+    //call scoring function
+    const runningTotal = calculateTotalScore()
+    console.log("total score: ", runningTotal)
   })
 })
 
@@ -99,6 +225,7 @@ containers.forEach(container => {
     e.preventDefault()
     const afterElement = sortDraggableElements(container, e.clientX)
     const draggable = document.querySelector(".dragging")
+
     if (afterElement == null) {
       container.appendChild(draggable)
     } else {
@@ -195,8 +322,6 @@ containers.forEach(container => {
       })
     })
     submitButtonGraphics()
-    // call attempt count
-    // updateActivityValue()
 
     //call scoring function
     const runningTotal = calculateTotalScore()
@@ -224,6 +349,8 @@ function sortDraggableElements(container, x) {
   ).element
 }
 
+// mobile sorting logic--------------------------------------------------------------------------------
+
 export {
   draggables,
   containers,
@@ -234,30 +361,3 @@ export {
   pointsPossible,
   resultsModal,
 }
-
-// let activityValue = 0
-
-// function updateActivityValue() {
-//   const draggableItems = Array.from(document.querySelectorAll(".draggable"))
-//   draggableItems.forEach(draggableItem => {
-//     if (draggableItem.classList.contains("dragging")) {
-//       draggableItem.dataset.activity = activityValue.toString()
-//       activityValue++
-//     }
-//   })
-//   console.log(activityValue)
-// }
-
-// function highestActivityValue() {
-//   const draggables = Array.from(document.querySelectorAll(".draggable"))
-//   let maxActivityValue = 0
-
-//   draggables.forEach(draggable => {
-//     const activityValue = parseInt(draggable.dataset.activity)
-
-//     if (activityValue > maxActivityValue) {
-//       maxActivityValue = activityValue
-//     }
-//   })
-//   return maxActivityValue
-// }
